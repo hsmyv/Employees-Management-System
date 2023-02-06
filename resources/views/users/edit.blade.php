ff507928a4abb0e8bce615a163a61df4fbcb4396
Hasan <hsmusayev@gmail.com>
2023-02-06 11:38:57 +0400

    updated

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -9,7 +9,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Update User') }} <a href="{{route('users.index')}}" class="float-right">Back</a></div>
+                <div class="card-header">{{ __('Update User') }}
+                    <a href="{{route('users.index')}}" class="float-right">Back</a>
+                </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('users.update', $user->id) }}">
@@ -82,8 +84,49 @@
                 <form method="POST" action="{{route('users.destroy', $user->id)}}">
                     @csrf
                     @method('DELETE')
-                    <button class="btn btn-danger">Delete {{$user->name}}</button>
+                    <button class="btn btn-danger">Delete {{$user->username}}</button>
                 </form>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Change Password') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('users.change.password', $user->id)}}">
+                        @csrf
+
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Update password') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
